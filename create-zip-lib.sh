399309d93666f94197cd4d526c88671871f598cd
Oleg Pashkevych <vertigo.if@gmail.com>
2018-11-21 11:28:59 +0200

    Zip script is tuned

--- create-zip-lib.sh
+++ create-zip-lib.sh
@@ -2,5 +2,5 @@
 rm RoboCar.zip
 cd ..
 find RoboCar/ -path '*/.*' -prune -o -type f \( -iname "*.*" ! -iname "*.sh" ! -iname "*.zip" \) -print | zip RoboCar.zip -@
-cp RoboCar.zip RoboCar
+mv RoboCar.zip RoboCar
 cd RoboCar
